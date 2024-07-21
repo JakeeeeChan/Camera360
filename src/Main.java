fb09f3244b049749e451d8ff8a90e0553e9a06a9
JakeeeeChan <jakesanuk@gmail.com>
2024-07-21 09:14:58 +0700

    Second working build without GUI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("test");
+    private static String folderPath = "C:\\Users\\jakes\\Documents\\JakesStuff\\Coding\\Java\\Camera360\\photos\\";
+
+    private  static String path = "C:\\Users\\jakes\\Documents\\JakesStuff\\Coding\\Java\\Camera360\\";
+
+    public static void main(String[] args) throws IOException {
+
+        //new GUI(folderPath);
+
+        new CompileImage(folderPath, path);
     }
 }
---
+++ src/SliceImage.java
@@ -0,0 +1,47 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SliceImage {
+
+    public int sliceWidth;
+
+    public SliceImage(String folderPath, String path) throws IOException {
+        ImageProperties imgp = new ImageProperties(folderPath);
+        File [] files = imgp.files;
+        int height = imgp.height;
+        int width = imgp.width;
+        int posX = width/2;
+        int posY = 0;
+        int sliceWidth = 3;
+
+
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isFile()){
+                //slice image and export as new
+                BufferedImage bimg = ImageIO.read(files[i]);
+                BufferedImage slicedImage = bimg.getSubimage(posX, posY, sliceWidth, height);
+
+                //check if dir is already created for sliced images
+                if (Files.exists(Path.of(path + "\\slicedImages\\"))) {
+                    File slicedPath = new File(path + "\\slicedImages\\");
+
+                    File outputFile = new File(path + "\\slicedImages\\" + Integer.toString(i) + ".png");
+
+                    ImageIO.write(slicedImage, "png", outputFile);
+                } else {
+                    new File(path + "\\slicedImages\\").mkdirs();
+
+                    File outputFile = new File(path + "\\slicedImages\\" + Integer.toString(i) + ".png");
+
+                    ImageIO.write(slicedImage, "png", outputFile);
+                }
+            }
+        }
+
+        this.sliceWidth = sliceWidth;
+    }
+}
